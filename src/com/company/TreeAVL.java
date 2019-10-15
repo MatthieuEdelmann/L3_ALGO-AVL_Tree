0b22cf445182022a0d719b3bfd2baf6dc63535af
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2019-10-15 20:59:31 +0200

    azerrty

--- src/com/company/TreeAVL.java
+++ src/com/company/TreeAVL.java
@@ -1,81 +1,75 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TreeAVL {
     public int height;
     public NodeAVL root;
 
     public TreeAVL() {
-        this.height = 0;
+        this.height = 1;
         this.root = null;
     }
 
-
-
-
-    //
-    public boolean isExistNode(NodeAVL n) {
-        if (n == null) {
-            return false;
+    //ajoute ou supprime le noeud de l'arbre
+    public void addOrDeleteNode(NodeAVL actualNode, NodeAVL newNode){
+        if (isExistNode(newNode)){  //si le noeud est presnt
+            removeNode(actualNode,newNode); //on retire ce noeud
+            //equilibrage
         }
-        if (n.getValueNode() == n.getLeft().getValueNode() || n.getValueNode() == n.getRight().getValueNode()) {
-            return true;
-        }
-
-        if (n.getValueNode() < n.getRight().getValueNode()) {
-            return isExistNode(n.getLeft());
-        }
-
-        return isExistNode(n.getRight());
+        else    //sinon on ajoute ce noeud
+            addNode(actualNode,newNode); //ajout du noeud
+            //equillibrage
     }
 
-
-    /*// retourne la hauteur de l'arbre
-    // a finir
-    public int getHeight(TreeAVL a) {
-        int result = 0;
-        if (a != null) {
-            int heightL = getHeight(a.);//
-            int heightR = getHeight(a.);//
-            int maxHeightSoon = (heightL > heightR ? heightL : heightR);
-            result = 1 + maxHeightSoon;
-            a.height = result;
+    public void removeNode(NodeAVL actualNode, NodeAVL deleteNode){
+        if (deleteNode.getValueNode() < actualNode.getValueNode()){
+            //deleteNode.setLeft(removeNode(actualNode,deleteNode.getLeft()));
         }
-        return result;
-    }*/
+        else if (deleteNode.getValueNode() > actualNode.getValueNode()){
+            //deleteNode.setRight(removeNode(actualNode,deleteNode.getRight()));
+            ////
+        }
+        else if (actualNode.getLeft()==null){
+            height--;
+            actualNode.setRight(null);
+        }
+        else if (actualNode.getRight()==null){
+            height--;
+            actualNode.setLeft(null);
+        }
+    }
 
-    public void addOoDeleteNode(NodeAVL actualNode, NodeAVL newNode){
-        if (actualNode == null) {
-            root = newNode;
+    public void addNode(NodeAVL actualNode, NodeAVL newNode){
+        // si l'arbre est vide on insert le nouveau node comme racine
+        if (isEmpty()) {
+            actualNode = newNode;
             height = 1;
             return;//Root
-        } else if (actualNode.getValueNode() == newNode.getValueNode()) {
-            //delete
         }
-        else if (actualNode.getValueNode() > newNode.getValueNode()){
+        //verifie si le noeud a un fils gauche
+        if (actualNode.getValueNode() > newNode.getValueNode()){
             if (actualNode.getLeft()==null) {
                 actualNode.setLeft(newNode);
                 height++;
                 return;
             }
-            addOoDeleteNode(actualNode.getLeft(), newNode);
+            addNode(actualNode.getLeft(), newNode);
         }
+        // pareil mais pour le fils droit
         else {
-            if (actualNode.getRight()==null) {
+            if (actualNode.getRight() == null) {
                 actualNode.setRight(newNode);
                 height++;
                 return;
             }
-            addOoDeleteNode(actualNode.getRight(), newNode);
+
+            addNode(actualNode.getRight(), newNode);
         }
     }
 
-    //public static removeNode(NodeAVL n){
+    public void balanceTree(TreeAVL a){
 
-    //}
-
+    }
+//affiche
     public void display(NodeAVL actualNode){
         //System.out.print(actualNode.getValueNode()+" ");
         if (actualNode.getRight() == null && actualNode.getLeft() == null);
@@ -93,21 +87,36 @@ public class TreeAVL {
         return;
     }
 
-    /*public void display(NodeAVL actualNode){
-        //System.out.print(actualNode.getValueNode()+" ");
-        boolean leftNodeIsExist = false, rightNodeIsExist = false;
-        if (actualNode.getLeft() != null){
-            leftNodeIsExist = true;
-            System.out.print(actualNode.getLeft().getValueNode()+" ");
-        } else System.out.print("null ");
-        if (actualNode.getRight() != null){
-            rightNodeIsExist = true;
-            System.out.print(actualNode.getRight().getValueNode()+" ");
-        } else System.out.print("null ");
 
-        if (leftNodeIsExist) display(actualNode.getLeft());
-        if (rightNodeIsExist) display(actualNode.getRight());
-        return;
-    }*/
+    // isEmpty fonction boolean qui retourne l'etat de l'arbre
+    public boolean isEmpty(){
+
+        return (this.root==null);
+    }
+
+// isExistNode permet de savoir si une valeur est dans l'arbre
+    public boolean isExistNode(NodeAVL n) {
+        if (n == null) {
+            return false;
+        }
+        if (n.getValueNode() == root.getLeft().getValueNode() || n.getValueNode() == root.getRight().getValueNode()) {
+            return true;
+        }
+
+        if (n.getValueNode() < root.getRight().getValueNode()) {
+            return isExistNode(root.getLeft());
+        }
+
+        return isExistNode(root.getRight());
+    }
+
+    // retourne la hauteur de l'arbre
+    public int getHeight(TreeAVL a) {
+
+        return height;
+    }
+
+
+
 
 }
