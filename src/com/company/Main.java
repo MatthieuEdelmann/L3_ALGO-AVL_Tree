908af4f68bc105c7ac2a55d22ce2d5dd1ee3ace7
Matthieulele <45042711+Matthieulele@users.noreply.github.com>
2019-10-15 22:21:30 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         TreeAVL tree = new TreeAVL();
-            tree.addOoDeleteNode(tree.root, new NodeAVL(50));
-            tree.addOoDeleteNode(tree.root, new NodeAVL(45));
-            tree.addOoDeleteNode(tree.root, new NodeAVL(56));
-            tree.addOoDeleteNode(tree.root, new NodeAVL(5));
-            tree.addOoDeleteNode(tree.root, new NodeAVL(54));
-        System.out.println(tree.root.getValueNode());
-            tree.display(tree.root);
+        tree.addOoDeleteNode(tree.root, new NodeAVL(50));
+        tree.addOoDeleteNode(tree.root, new NodeAVL(45));
+        tree.addOoDeleteNode(tree.root, new NodeAVL(56));
+        tree.addOoDeleteNode(tree.root, new NodeAVL(5));
+        tree.addOoDeleteNode(tree.root, new NodeAVL(54));
+        tree.display();
     }
 }
